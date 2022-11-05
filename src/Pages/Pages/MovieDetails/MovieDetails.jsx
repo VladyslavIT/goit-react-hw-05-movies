3582b897e6e8ea3a64f93ce2ded4d457f8955bd0
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-11-05 20:17:07 +0200

    fix

--- src/Pages/Pages/MovieDetails/MovieDetails.jsx
+++ src/Pages/Pages/MovieDetails/MovieDetails.jsx
@@ -15,7 +15,7 @@ const MovieDetails = () => {
     if (!currentMovie) {
         return;
     }
-    
+
     const { title, poster_path, vote_average, tagline, overview, genres } = currentMovie;
     return (
         <div>
@@ -51,28 +51,3 @@ const MovieDetails = () => {
 
 export { MovieDetails };
 
-// title, poster_path, vote_average, tagline, overview, genres
-
-// <StyledLinkBack to={backLinkHref}>Go back</StyledLinkBack>
-    //   <Box>
-    //     <div>
-    //       <Img src={`${movieImg(poster_path)}`} alt="title" />
-    //     </div>
-
-    //     <MovieInfo>
-    //       <MainTitle>{title}</MainTitle>
-    //       <Tagline>{tagline}</Tagline>
-    //       <Score>
-    //         {' '}
-    //         <span>User Score:</span> {`${vote_average.toFixed(1)}`}
-    //       </Score>
-    //       <Genres>Genres</Genres>
-    //       <GenresList>
-    //         {genres.map(({ id, name }) => (
-    //           <li key={id}>- {name}</li>
-    //         ))}
-    //       </GenresList>
-    //       <Overview>Overview</Overview>
-    //       <p>{overview}</p>
-    //     </MovieInfo>
-    //   </Box>
