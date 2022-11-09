2cad981fcb906724debf5e633332f7e5266356f9
VladyslavIT <98426939+VladyslavIT@users.noreply.github.com>
2022-11-09 16:15:33 +0000

    Deploying to gh-pages from @ VladyslavIT/goit-react-hw-05-movies@e8739a63ffcb1ed82da00d16d2e7bb7b5d0fdb78 🚀

---
+++ static/js/567.72323a48.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[567],{6567:function(e,t,n){"use strict";n.r(t);var r=n(3164),c=n(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(r.Searchbar,{})})}},3164:function(){}}]);
+//# sourceMappingURL=567.72323a48.chunk.js.map
